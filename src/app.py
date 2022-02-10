76f9b7ea6a589f4ce737447a4531b8b14eb05671
krog <virtualspook@gmail.com>
2022-02-10 18:20:32 +0600

    handle 'name' query string

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from werkzeug.wrappers import Response
+from werkzeug.wrappers import Request, Response
 
 
 def application(env, start_response):
-    response = Response('Hello, World!', mimetype='text/plain')
+    request = Request(env)
+    text = f"Hello, {request.args.get('name', 'World')}!"
+    response = Response(text, mimetype='text/plain')
     return response(env, start_response)
