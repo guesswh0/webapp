403555cae35b404535adab5aad3ea5ac30f5830e
krog <virtualspook@gmail.com>
2022-02-11 10:00:17 +0600

    test user

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,8 @@ def test_default(client):
     response = client.get('/')
     assert response.status == '200 OK'
     assert response.data == b'Hello, World!'
+
+def test_name(client):
+    response = client.get('/?name=Test')
+    assert response.status == '200 OK'
+    assert response.data == b'Hello, Test!'
