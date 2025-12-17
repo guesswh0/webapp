228408714f20d9bdc599a75bef04326c2b9664fe
dank0 <ohw0sseug@gmail.com>
2025-12-17 17:12:02 +0500

    implement graceful shutdown for the server

--- src/run.py
+++ src/run.py
@@ -1,12 +1,26 @@
+import signal
+import sys
 from wsgiref.simple_server import make_server
 
 from app import application
 
+
+def _graceful_shutdown(signum, frame):
+    time.sleep(10)  # simulate cleanup tasks
+    sys.exit(0)
+
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, _graceful_shutdown)
+    signal.signal(signal.SIGINT, _graceful_shutdown)
+
     with make_server('', 8080, application) as httpd:
         print(" * Serving on http://{0}:{1} (Press CTRL+C to quit)"
               .format(*httpd.server_address))
         try:
             httpd.serve_forever()
-        except KeyboardInterrupt:
+        finally:
+            # Ensure clean shutdown
+            print(" * Shutting down server...")
             httpd.server_close()
+            print(" * Server shutdown complete")
